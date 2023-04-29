ed9b639529d3ed73b692b3c0e4fe8c2cdadae6ef
Roman Zajavkowy <lewy007@o2.pl>
2023-04-29 13:25:58 +0200

    commmit-3 from git_tutorial-zajavka2

--- Person.java
+++ Person.java
@@ -2,5 +2,6 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello2");
+        System.out.println("Hello3");
     }
 }
