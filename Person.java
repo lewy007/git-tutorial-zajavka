9599d181835bda88e2f8a91056c76ba371d88e64
Roman Zajavkowy <lewy007@o2.pl>
2023-04-29 18:15:21 +0200

    Person 1

--- Person.java
+++ Person.java
@@ -2,7 +2,7 @@ public class Person {
     public static void main(String[] args) {
         // comment
         System.out.println("Hello");
-        System.out.println("Hello2");
-        System.out.println("Hello3");
+        System.out.println("Hello");
+
     }
 }
